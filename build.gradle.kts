2f03127f46e0ef941c30e7de05f1b9cf6148ae32
Yuretsu <elizabethquach@live.ca>
2023-04-10 09:53:14 -0400

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.discord4j:discord4j-core:3.2.5-SNAPSHOT")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
